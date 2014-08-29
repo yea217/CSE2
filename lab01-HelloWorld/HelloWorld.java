5dd6cc18f7ac9aaf69c3752bddfda63125ac5039
yea217 <yea217@lehigh.edu>
2014-08-29 14:48:52 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////
+// Yehia Awad
+//lab 01
+// Hello World Java Program
+// yea217
+// cse2
+
+// define a class
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args){
+            
+//print out statement with box around it
+System.out.println("########################");
+System.out.println("########################"); 
+System.out.println("#                      #");
+System.out.println("# Hello   World   2014!#");
+System.out.println("#                      #");
+System.out.println("########################");
+System.out.println("########################");
+System.out.println("test");
+
+//test/testtest
+    }
+}
