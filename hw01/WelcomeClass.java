690073537f17f203b4ba3372cf03f3607e794870
yea217 <yea217@lehigh.edu>
2014-08-31 23:12:08 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+////////////
+//Yehia Awad
+//hw01
+//cse2
+//yea217
+
+
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args){
+            
+//print out statement with box around it
+System.out.println("    -----------");
+System.out.println("    | WELCOME |"); 
+System.out.println("    -----------");
+System.out.println("  ^  ^  ^  ^  ^  ^");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+System.out.println("|-Y--E--A--2--1--7-|");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println("  v  v  v  v  v  v ");
+System.out.println("Over the summer I spent my days working for a landscaping company.");
+
+    }
+}
